2a4193b09883f71e158c5b9220cc9aaa2399988d
George Cai <epicspartan601670@berkeley.edu>
2023-12-19 18:31:46 -0800

    new dolphins

--- src/densitymap_generator/h5_viewer.py
+++ src/densitymap_generator/h5_viewer.py
@@ -13,6 +13,11 @@ path5 = r"../../dataset/ground_truth_dot/B.h5"
 path6 = r"../../dataset/ground_truth_line/B.h5"
 path7 = r"../../dataset/ground_truth_circle/B.h5"
 
+path8 = r"../../dataset/ground_truth_anisotropy_1_4/C.h5"
+path9 = r"../../dataset/ground_truth_dot/C.h5"
+path10 = r"../../dataset/ground_truth_line/C.h5"
+path11 = r"../../dataset/ground_truth_circle/C.h5"
+
 with h5py.File(path5, "r") as hf:
     p = np.array(hf['density'])
 
